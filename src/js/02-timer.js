e6c7769928ce90c2d0ee9c929b568372f3d3811f
Brayan Medina <102181205+Brayanmv77@users.noreply.github.com>
2023-05-07 10:14:38 -0500

    Task 2 finished

---
+++ src/js/02-timer.js
@@ -0,0 +1,78 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const datetimePicker = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('[data-start]');
+
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate < new Date()) {
+      Notiflix.Notify.failure('Por favor elige una fecha en el futuro');
+      startButton.setAttribute('disabled', '');
+      return;
+    }
+    startButton.removeAttribute('disabled');
+  },
+};
+
+flatpickr(datetimePicker, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function startTimer() {
+  const selectedDate = new Date(datetimePicker.value);
+
+  const intervalId = setInterval(() => {
+    const timeRemaining = selectedDate.getTime() - new Date().getTime();
+
+    if (timeRemaining <= 0) {
+      clearInterval(intervalId);
+      daysElement.textContent = '00';
+      hoursElement.textContent = '00';
+      minutesElement.textContent = '00';
+      secondsElement.textContent = '00';
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(timeRemaining);
+
+    daysElement.textContent = addLeadingZero(days);
+    hoursElement.textContent = addLeadingZero(hours);
+    minutesElement.textContent = addLeadingZero(minutes);
+    secondsElement.textContent = addLeadingZero(seconds);
+  }, 1000);
+}
+
+startButton.addEventListener('click', startTimer);
